5ba398ac5183a6fe921bc72f881da41207ebcac5
Manoj Kumar <manojkumarkothapalli2002@gmail.com>
2025-03-08 18:38:28 +0530

    dynamic page commit

---
+++ dynamic-page/src/Contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Contact = () => (
+  <div className="container">
+    <h1>Contact Us</h1>
+    <p>You can contact me at naman@gmail.com.</p>
+  </div>
+);
---
+++ dynamic-page/src/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Home = () => (
+  <div className="container">
+    <h1>Welcome to the Home Page!</h1>
+    <p>This is a dynamic React application.</p>
+  </div>
+);
